36251cd6fc8d1b936f6d7eabb9c5012a06b8db35
mhmtkarakas <m.karakas32@gmail.com>
2022-12-05 15:29:54 +0300

    initialize project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Getting Started with Create React App
+
+<p>Daha önce javascript ile yapılmış olan TodoList projesi tekrar React ile yapıldı.</p>
+
+
+<img src="./Ekranresmi.png">
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Todo from './todo'
+
+function TodoList({todos,setTodos,filteredTodos}) {
+  return (
+    <div className='todo-container'>
+      <ul className="todo-list">
+        {filteredTodos.map((todo)=>(
+          <Todo
+          text={todo.text}
+          todo={todo}
+          key = {todo.id}  
+          todos={todos} 
+          setTodos={setTodos}       />
+        ))}
+     
+      </ul>
+    </div>
+  )
+}
+
+export default TodoList
+
